1165a16ff5492bdfe7e9d29432fbec74431ab3a8
SdeScotty <SdeScotty@hotmail.com>
2017-12-17 12:54:42 +0100

    Hace una suma

--- Proyecto1/newEmptyPHPWebPage.php
+++ Proyecto1/newEmptyPHPWebPage.php
@@ -10,8 +10,29 @@ and open the template in the editor.
         <title></title>
     </head>
     <body>
+        <form name="formulario" method="post" action="">
+            <table align="center" width="60%" heigth="100" border="2">
+                <tr align="center">
+                    <td bgcolor="blue" heigth="30">
+                        <input type="text" name="var1" size="10" value="Número 1"/>
+                    </td>
+                    <td bgcolor="red">
+                        <input type="text" name="var2" size="10" value="Numero 2" />
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="2" align="center"><input type="submit" name="enviar" value="ENVIAR"/></td>
+                </tr>
+            </table>
+        </form>
         <?php
-        // put your code here
+            
+            if (isset($_POST["enviar"])){
+                $num1=$_POST["var1"];
+                $num2=$_POST["var2"];
+                echo "<center><h1>La suma de $num1 y $num2 es ".($num1+$num2). "</h1></center>";
+            }
+            
         ?>
     </body>
 </html>
